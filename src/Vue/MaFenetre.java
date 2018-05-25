fe5db5c6a3f60af0fff3df78c00a152226cc7747
jonas <j0naas_14@hotmail.com>
2018-05-25 10:13:15 +0200

    Première Fenêtre

---
+++ src/Vue/MaFenetre.java
@@ -0,0 +1,18 @@
+/*
+* MaFenetre.java
+* Auteur : Jonas Pilloud
+* Date de création : 25 mai 2018
+*/
+
+package Vue;
+
+import javax.swing.JFrame;
+
+public class MaFenetre extends JFrame {
+	public MaFenetre() {
+		this.setSize(480, 800);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
